e9e8a40ef09bbf0646487af85874dc1212139c8b
sansi <396914701@qq.com>
2017-11-16 21:32:44 +0800

    2

--- data/net_turn_data/next_match.py
+++ data/net_turn_data/next_match.py
@@ -3,7 +3,7 @@ from data.spider_tool.common_tool import *
 
 
 def next_match_data():
-    # 第一轮32周  本赛季第一场ID 1190174
+    # 第一轮32周  本赛季第一场ID 1190174 1
     week = "32"
 
     url = 'https://www.whoscored.com/tournamentsfeed/15151/Fixtures/?d=2017W' + week + '&isAggregate=false'
