3f5ca1361f705800fdc78be6ade3c31ae64e8c30
396914701@qq.com <git35313617>
2017-11-18 23:27:26 +0800

    修改抓取

--- data/webdriver_spider/threesix_spider/premier_league/premier_threesix_page.py
+++ data/webdriver_spider/threesix_spider/premier_league/premier_threesix_page.py
@@ -7,23 +7,73 @@
 
 from selenium import webdriver
 import time
-
-# options = webdriver.ChromeOptions()
-# options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"')
-# options.add_argument('Host="www.bet365.com"')
-# options.add_argument('Referer="https://www.bet365.com/"')
-# options.add_argument('Cookie="aaat=am=0&at=00000000-0000-0000-0000-000000000000&ts=14-11-2017 02:21:26&v=2; bs=bt=1&mo=0&fs=0||&; session=processform=0&id=%7B4C37722F%2D0539%2D48CC%2D9096%2DC57356D1CB9A%7D; pstk=CBA025928D124ED8AF20329A6BB4C3A6000003; aps03=tzi=27&cg=0&ltwo=False&ao=1&cst=114&v=1&hd=Y&lng=10&cf=E&ct=42&oty=2&bst=1; rmbs=3; usdi=uqid=33465139%2DD6AC%2D4A63%2DA81B%2DFCBC35377EF2"')
+from bs4 import BeautifulSoup
+from data.spider_tool.proxy import *
+from selenium.webdriver.common.proxy import Proxy
 
 browser = webdriver.Chrome()
-browser.get('https://www.bet365.com/')
-#点击简体中文
-browser.find_element_by_css_selector("a[class=\"lpdgl\"]").click()
-#点击足球
-time.sleep(15)
+#进入列表页
+def match_list_html():
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    # }
 
-browser.switch_to_window(browser.window_handles[0])
-browser.find_element_by_css_selector("div.wn-FavouritesContainer  ~ div").click()
+    #ip_list_port = getIpList(headers)
+    #random_ip = getRandomIp(ip_list_port)
+    # chromeOptions = webdriver.ChromeOptions()
+    # chromeOptions.add_argument('--proxy-server=http://147.75.208.57:20000')
+    #
+    # browser = webdriver.Chrome(chrome_options=chromeOptions)
 
-html = browser.page_source
-#browser.close()
-print(html)
+    browser.get('https://www.bet365.com/')
+    try:
+        #点击简体中文
+        browser.find_element_by_css_selector("a[class=\"lpdgl\"]").click()
+
+        #点击足球
+        time.sleep(15)
+
+        browser.find_element_by_css_selector("div.wn-FavouritesContainer  ~ div").click()
+
+        #点击英超联赛
+        time.sleep(10)
+        element = browser.find_elements("css selector",".sm-Market ")[2]
+        element = element.find_elements("css selector",".sm-CouponLink_Label ")[0]
+        element.click()
+        time.sleep(10)
+
+        # 英超列表首页
+        elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
+        loop_data(elements,0)
+
+    except:
+        print("页面异常，请检查网页是否可以正常打开")
+        browser.close()
+
+#获取每场比赛信息
+def loop_data(elements,index):
+    try:
+        elements[index].click()
+        time.sleep(10)
+        print(browser.page_source)
+
+        element = browser.find_element("css selector", ".cl-BreadcrumbTrail_BackButton ")
+        element.click()
+        time.sleep(10)
+
+        elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
+        loop_data(elements,index + 3)
+    except:
+        print("赔率抓取完成")
+        browser.close()
+
+#獲取賠率信息
+def get_odd_info(html):
+    soup = BeautifulSoup(open("mainpage.html",'rb'), "html.parser")
+    print(soup)
+
+if __name__ == '__main__':
+    match_list_html()
+
+
+
