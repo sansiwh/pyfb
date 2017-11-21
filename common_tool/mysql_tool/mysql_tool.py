6615ab4ac8a4d71055624d067fa366aa6889c19c
sansi <396914701@qq.com>
2017-11-21 11:11:37 +0800

    插入数据库操作

--- common_tool/mysql_tool/mysql_tool.py
+++ common_tool/mysql_tool/mysql_tool.py
@@ -6,6 +6,8 @@ name_gid = {}
 cur = get_db_cur()
 sql = "select * from team_info"
 print("查询")
+
+snowflake.client.setup("localhost", 30001)
 try:
     cur.execute(sql)
     results = cur.fetchall()
@@ -32,18 +34,26 @@ def get_match_id_by_name(param):
     home_team_name = param["home_team_name"]
     away_team_name = param["away_team_name"]
     match_date = param["match_date"]
-    sql = "select * from match_info where "
+    home_gid = get_team_info_by_name(home_team_name)
+    away_gid = get_team_info_by_name(away_team_name)
+    sql = "select gid from match_info where match_date = '"+str(match_date)+"' and main_team_gid="+str(home_gid)+" and custom_team_gid="+str(away_gid)
     cur.execute(sql)
     results = cur.fetchall()
+    return results[0][0]
 
 def close_db():
     db.close()
 
-if __name__ == '__main__':
-    print(get_team_info_by_name("莱斯特城"))
-    snowflake.client.setup("localhost", 30001)
-    gid = snowflake.client.get_guid()
+def get_snowflake_gid():
+    return snowflake.client.get_guid()
 
-    #sql = "insert into match_info(gid,main_team_gid,custom_team_gid,league_team_gid,match_date,match_week,match_time,turn) values(4030163061145862145,375434198808264704,375434474885742592,375414018631794688,2017-08-12,星期六,4:3,1)"
-    sql = "insert into match_info(gid,main_team_gid,custom_team_gid,league_gid,match_date,match_week,match_time,turn) values(4030173254533513217,375434198808264704,375434474885742592,375414018631794688,'2017-08-12','星期六','4:3',1)"
-    insert(sql)
+if __name__ == '__main__':
+    # print(get_team_info_by_name("莱斯特城"))
+    # snowflake.client.setup("localhost", 30001)
+    # gid = snowflake.client.get_guid()
+    #
+    # #sql = "insert into match_info(gid,main_team_gid,custom_team_gid,league_team_gid,match_date,match_week,match_time,turn) values(4030163061145862145,375434198808264704,375434474885742592,375414018631794688,2017-08-12,星期六,4:3,1)"
+    # sql = "insert into match_info(gid,main_team_gid,custom_team_gid,league_gid,match_date,match_week,match_time,turn) values(4030173254533513217,375434198808264704,375434474885742592,375414018631794688,'2017-08-12','星期六','4:3',1)"
+    # insert(sql)
+    param = {'away_team_name': '斯托克城', 'home_team_name': '布莱顿', 'match_date': '2017-11-21'}
+    get_match_id_by_name(param)
--- data/webdriver_spider/threesix_spider/premier_league/premier_threesix_page.py
+++ data/webdriver_spider/threesix_spider/premier_league/premier_threesix_page.py
@@ -13,65 +13,65 @@ from selenium.webdriver.common.proxy import Proxy
 import datetime
 from common_tool.mysql_tool.mysql_tool import *
 
-# browser = webdriver.Chrome()
-# #进入列表页
-# def match_list_html():
-#     # headers = {
-#     #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
-#     # }
-#
-#     #ip_list_port = getIpList(headers)
-#     #random_ip = getRandomIp(ip_list_port)
-#     # chromeOptions = webdriver.ChromeOptions()
-#     # chromeOptions.add_argument('--proxy-server=http://147.75.208.57:20000')
-#     #
-#     # browser = webdriver.Chrome(chrome_options=chromeOptions)
-#
-#     browser.get('https://www.bet365.com/')
-#     try:
-#         #点击简体中文
-#         browser.find_element_by_css_selector("a[class=\"lpdgl\"]").click()
-#
-#         #点击足球
-#         time.sleep(15)
-#
-#         browser.find_element_by_css_selector("div.wn-FavouritesContainer  ~ div").click()
-#
-#         #点击英超联赛
-#         time.sleep(10)
-#         element = browser.find_elements("css selector",".sm-Market ")[2]
-#         element = element.find_elements("css selector",".sm-CouponLink_Label ")[0]
-#         element.click()
-#         time.sleep(10)
-#
-#         # 英超列表首页
-#         elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
-#         loop_data(elements,0)
-#
-#     except:
-#         print("页面异常，请检查网页是否可以正常打开")
-#         browser.close()
-#
-# #获取每场比赛信息
-# def loop_data(elements,index):
-#     try:
-#         elements[index].click()
-#         time.sleep(10)
-#
-#         #点击全部比分
-#         element = browser.find_elements("css selector", ".gl-MarketGroup_BBarItem ")
-#         element[1].click()
-#         print(browser.page_source)
-#
-#         element = browser.find_element("css selector", ".cl-BreadcrumbTrail_BackButton ")
-#         element.click()
-#         time.sleep(10)
-#
-#         elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
-#         loop_data(elements,index + 3)
-#     except:
-#         print("赔率抓取完成")
-#         browser.close()
+browser = webdriver.Chrome()
+#进入列表页
+def match_list_html():
+    # headers = {
+    #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+    # }
+
+    #ip_list_port = getIpList(headers)
+    #random_ip = getRandomIp(ip_list_port)
+    # chromeOptions = webdriver.ChromeOptions()
+    # chromeOptions.add_argument('--proxy-server=http://147.75.208.57:20000')
+    #
+    # browser = webdriver.Chrome(chrome_options=chromeOptions)
+
+    browser.get('https://www.bet365.com/')
+    try:
+        #点击简体中文
+        browser.find_element_by_css_selector("a[class=\"lpdgl\"]").click()
+
+        #点击足球
+        time.sleep(15)
+
+        browser.find_element_by_css_selector("div.wn-FavouritesContainer  ~ div").click()
+
+        #点击英超联赛
+        time.sleep(10)
+        element = browser.find_elements("css selector",".sm-Market ")[2]
+        element = element.find_elements("css selector",".sm-CouponLink_Label ")[0]
+        element.click()
+        time.sleep(10)
+
+        # 英超列表首页
+        elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
+        loop_data(elements,0)
+
+    except:
+        print("页面异常，请检查网页是否可以正常打开")
+        browser.close()
+
+#获取每场比赛信息
+def loop_data(elements,index):
+    try:
+        elements[index].click()
+        time.sleep(10)
+
+        #点击全部比分
+        element = browser.find_elements("css selector", ".gl-MarketGroup_BBarItem ")
+        element[1].click()
+        print(browser.page_source)
+
+        element = browser.find_element("css selector", ".cl-BreadcrumbTrail_BackButton ")
+        element.click()
+        time.sleep(10)
+
+        elements = browser.find_elements_by_css_selector('.sl-CouponFixtureLinkParticipant ')
+        loop_data(elements,index + 3)
+    except:
+        print("赔率抓取完成")
+        browser.close()
 
 #获取賠率信息并保存
 def get_odd_info():
@@ -96,28 +96,47 @@ def get_odd_info():
     type_1 = three_odd_list[3].get_text()
     type_0 = three_odd_list[5].get_text()
 
+    #插入胜负赔率
+    gid = get_snowflake_gid()
+    sql = "insert into three_odd_info (gid,match_gid,type,odd) values (" + str(gid) + "," + str(match_id) + ",3," + str(type_3)+")"
+    insert(sql)
+
+    gid = get_snowflake_gid()
+    sql = "insert into three_odd_info (gid,match_gid,type,odd) values (" + str(gid) + "," + str(match_id) + ",1," + str(type_1) + ")"
+    insert(sql)
+
+    gid = get_snowflake_gid()
+    sql = "insert into three_odd_info (gid,match_gid,type,odd) values (" + str(gid) + "," + str(match_id) + ",0," + str(type_0) + ")"
+    insert(sql)
+
     win_ping_divs = soup.find_all(class_="gl-Market3 gl-Market_General gl-Market_PWidth-33-3333 ")
     #主胜赔率
     socre_odd_win = win_ping_divs[0].find_all(class_="gl-ParticipantCentered gl-Participant_General gl-ParticipantCentered_NoHandicap ")
     for i in socre_odd_win:
-        type = 3
+        gid = get_snowflake_gid()
         score = i.find(class_="gl-ParticipantCentered_Name").get_text()
         odd = i.find(class_="gl-ParticipantCentered_Odds").get_text()
+        sql = "insert into three_odd_info (gid,match_gid,type,score,odd) values (" + str(gid) + "," + str(match_id) + ",3,"+str(score)+ "," + str(odd) + ")"
+        insert(sql)
 
     # 平局赔率
     socre_odd_ping = win_ping_divs[1].find_all(class_="gl-ParticipantCentered gl-Participant_General gl-ParticipantCentered_NoHandicap ")
     for i in socre_odd_ping:
-        type = 1
+        gid = get_snowflake_gid()
         score = i.find(class_="gl-ParticipantCentered_Name").get_text()
         odd = i.find(class_="gl-ParticipantCentered_Odds").get_text()
+        sql = "insert into three_odd_info (gid,match_gid,type,score,odd) values (" + str(gid) + "," + str(match_id) + ",1," + str(score) + "," + str(odd) + ")"
+        insert(sql)
 
     # 客胜赔率
     lose_divs = soup.find(class_="gl-Market3 gl-Market_General gl-Market_PWidth-33-3333 gl-Market_LastInRow ")
     socre_odd_lose = lose_divs.find_all(class_="gl-ParticipantCentered gl-Participant_General gl-ParticipantCentered_NoHandicap ")
     for i in socre_odd_lose:
-        type = 0
+        gid = get_snowflake_gid()
         score = i.find(class_="gl-ParticipantCentered_Name").get_text()
         odd = i.find(class_="gl-ParticipantCentered_Odds").get_text()
+        sql = "insert into three_odd_info (gid,match_gid,type,score,odd) values (" + str(gid) + "," + str(match_id) + ",0," + str(score) + "," + str(odd) + ")"
+        insert(sql)
 
 
 if __name__ == '__main__':
